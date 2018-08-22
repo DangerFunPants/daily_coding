bccd91358bc6dee35a7cc0a570142f41c2afa723
Alexander James <alexander.james@ucalgary.ca>
2018-08-21 19:47:35 -0600

    Playing around with graph search in haskell

--- graph_search/search.hs
+++ graph_search/search.hs
@@ -1,71 +1,43 @@
-{-# LANGUAGE KindSignatures #-}
+import Control.Lens
 
-import qualified Data.Map as M
-import Data.List (sortOn)
-
-class PriorityQueue (m :: * -> *) where
-    pop :: Ord a => m a -> (a, (m a))
-    push :: Ord a => m a -> a -> m a
-    mkEmpty :: m a
-    isEmpty :: Ord a => m a -> Bool
-
-instance PriorityQueue ([]) where
-    pop q = (v, newQ)
-        where
-            v = minimum q
-            newQ = filter (/= v) q
-    push pq a = a:pq
-    mkEmpty = []
-    isEmpty = null
-
--- PSEUDOCODE: 
---
--- pred <- { a : None for a in verts }      // denote the immediante predecessor of each node
--- dist <- { a : inf for a in verts }       // denote the distance from the source node to every other node.
--- 
---
--- q <- insert src empty                    // insert the source node as the only node with non-inf dist in the q.
---
--- for v in verts if v is not src:
---      q <- insert q (v, dist[v])
---
--- while not (isEmpty q):
---      closest <- pop q                    // the closest node that we know about
---      nextHops <- neighbours closest      // Nodes seperated from closest by a single hop [(NodeId, EdgeWeight)]
---
---      for (id, w) in nextHops:
---          if dist[closest] + w < dist[id]:
---              dist[id] = dist[closest] + w
---              pred[id] = closest
---          
--- return (dist, pred)
---
-firstSatisfying :: (a -> Bool) -> [a] -> a
-firstSatisfying pred as = res
+bellmanFord' :: Int -> [Int] -> [Int] -> [(Int, Int, Int)] -> ([Int], [Int])
+bellmanFord' 0 dist preds _ = (dist, preds)
+bellmanFord' c dist preds es = bellmanFord' (pred c) newDist newPred es
     where
-        poss = filter pred as
-        res = head poss
+        newDist = foldl (\ds (u, v, w) -> let newV = (dist !! (pred u)) + w
+                                              oldV = (dist !! (pred v))
+                                          in if newV < oldV
+                                                then (element (pred v) .~ newV) ds
+                                                else ds) dist es
+        newPred = foldl (\ps (u, v, w) -> let newV = (dist !! (pred u)) + w
+                                              oldV = (dist !! (pred v))
+                                          in if newV < oldV
+                                                 then (element (pred v) .~ u) ps
+                                                 else ps) preds es
 
-at :: [(Int, Int)] -> Int -> Int
-ds `at` v = res
-    res = head $ filter (\(k, _) -> k == v) ds
-    
-
-dijkstra :: [(Int, Int)] -> [(Int, Int)] -> [Int] -> [(Int, (Int, Int))] -> ([(Int, Int)], [(Int, Int)])
-dijkstra dist pred [] _ = (dist, pred)
-dijkstra dist pred pq es = ([], [])
+bellmanFord :: [(Int, Int, Int)] -> [Int] -> Int -> ([Int], [Int])
+bellmanFord es vs s = bellmanFord' (succ vertC) dist (replicate vertC 0) es
     where
-        s = sortOn (\v -> snd (firstSatisfying (\a -> (fst a) == v) dist)) pq
-        minNode = head s
-        neighbours = filter (\(v, _) -> v == minNode) es
-        toUpdate =  
+        vertC = length vs
+        dist = [ if n == s then 0 else 1000000000 | n<-[1..vertC] ]
 
+addEdge :: (Int, Int, Int) -> [(Int, Int, Int)] -> [(Int, Int, Int)]
+addEdge e@(u, v, w) es = e:((v, u, w):es)
 
+testBF :: IO ()
+testBF = do
+    let ts = [ (1, 2, 1)
+             , (1, 3, 1)
+             , (2, 4, 1)
+             , (2, 3, 1)
+             , (3, 5, 1)
+             , (4, 5, 1)
+             , (4, 6, 1)
+             ]
+        es = foldl (\l t -> addEdge t l) [] ts
+        res = bellmanFord ts [1..6] 1
+    putStrLn $ show es
+    putStrLn $ show res
 
-
-
-
-
-
-
-        
+main :: IO ()
+main = testBF
