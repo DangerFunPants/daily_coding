6a97f2d88b53f19391827ddb161c164b0e4490d5
Alexander James <alexander.james@ucalgary.ca>
2018-09-07 12:08:58 -0600

    Get actual September 7 problem back.

--- daily_problems/haskell/september/september_7.hs
+++ daily_problems/haskell/september/september_7.hs
@@ -1,27 +1,47 @@
--- This question was asked by Google.
+-- This problem was asked by LinkedIn.
 -- 
--- Given an integer n and a list of integers l, write a function that randomly 
--- generates a number from 0 to n-1 that isn't in l (uniform).
+-- Determine whether a tree is a valid binary search tree.
+-- 
+-- A binary search tree is a tree with two children, left and right, and 
+-- satisfies the constraint that the key in the left child must be less than or 
+-- equal to the root and the key in the right child must be greater than or 
+-- equal to the root.
 
-import System.Random
 import Text.Show.Pretty
 
-genRandom' :: (Random a, Num a) => a -> IO a
-genRandom' n = getStdRandom $ randomR (0, n)
+data BTree a 
+    = Node a (BTree a) (BTree a)
+    | Nil
+    deriving (Show, Eq)
 
-genRandom :: Int -> [Int] -> IO Int
-genRandom n l = do
-    r <- genRandom' n
-    if r `elem` l
-        then genRandom n l
-        else return r
+isValidBst (Node v lc rc) f = 
+    if f v
+        then isValidBst lc (<= v) && isValidBst rc (>= v)
+        else False
+isValidBst Nil _ = True
 
 test1 :: IO ()
 test1 = do
-    let trialCount = 100000
-    vs <- mapM (\_ -> genRandom 10 [2, 3]) [1..trialCount]
-    let hist = fmap (\v -> (v, ((fromIntegral . length) $ filter (==v) vs) / trialCount))  [0..10]
-    putStrLn $ ppShow hist
+    let ts = Node 10
+                (Node 5
+                    (Node 3 Nil Nil)
+                    (Node 6 Nil Nil))
+                (Node 20
+                    (Node 15 Nil Nil)
+                    (Node 25 Nil Nil))
+    let ts' = Node 10
+                 (Node 5
+                     (Node 3 Nil Nil)
+                     (Node 6 Nil Nil))
+                 (Node 20
+                     (Node 30 Nil Nil)
+                     (Node 25 Nil Nil))
+        res = isValidBst ts (\_ -> True)	
+        res' = isValidBst ts' (\_ -> True)
+    putStrLn $ ppShow ts
+    putStrLn $ show res
+    putStrLn $ ppShow ts'
+    putStrLn $ show res'
 
 main :: IO ()
 main = test1
