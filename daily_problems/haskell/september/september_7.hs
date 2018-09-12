b1eee9f0a84209a322cd575e63006ae0c60d31ee
Alexander James <alexander.james@ucalgary.ca>
2018-09-11 21:01:06 -0600

    September 7 problem in haskell. Potentially infinite runtime, probably a better solution...

--- daily_problems/haskell/september/september_7.hs
+++ daily_problems/haskell/september/september_7.hs
@@ -1,47 +1,27 @@
--- This problem was asked by LinkedIn.
+-- This question was asked by Google.
 -- 
--- Determine whether a tree is a valid binary search tree.
--- 
--- A binary search tree is a tree with two children, left and right, and 
--- satisfies the constraint that the key in the left child must be less than or 
--- equal to the root and the key in the right child must be greater than or 
--- equal to the root.
+-- Given an integer n and a list of integers l, write a function that randomly 
+-- generates a number from 0 to n-1 that isn't in l (uniform).
 
+import System.Random
 import Text.Show.Pretty
 
-data BTree a 
-    = Node a (BTree a) (BTree a)
-    | Nil
-    deriving (Show, Eq)
+genRandom' :: (Random a, Num a) => a -> IO a
+genRandom' n = getStdRandom $ randomR (0, n)
 
-isValidBst (Node v lc rc) f = 
-    if f v
-        then isValidBst lc (<= v) && isValidBst rc (>= v)
-        else False
-isValidBst Nil _ = True
+genRandom :: Int -> [Int] -> IO Int
+genRandom n l = do
+    r <- genRandom' n
+    if r `elem` l
+        then genRandom n l
+        else return r
 
 test1 :: IO ()
 test1 = do
-    let ts = Node 10
-                (Node 5
-                    (Node 3 Nil Nil)
-                    (Node 6 Nil Nil))
-                (Node 20
-                    (Node 15 Nil Nil)
-                    (Node 25 Nil Nil))
-    let ts' = Node 10
-                 (Node 5
-                     (Node 3 Nil Nil)
-                     (Node 6 Nil Nil))
-                 (Node 20
-                     (Node 30 Nil Nil)
-                     (Node 25 Nil Nil))
-    res = isValidBst ts (\_ -> True)	
-    res' = isValidBst ts' (\_ -> True)
-putStrLn $ ppShow ts
-        res = isValidBst ts (\_ -> True)
-    putStrLn $ ppShow ts
-    putStrLn $ show res
+    let trialCount = 100000
+    vs <- mapM (\_ -> genRandom 10 [2, 3]) [1..trialCount]
+    let hist = fmap (\v -> (v, ((fromIntegral . length) $ filter (==v) vs) / trialCount))  [0..10]
+    putStrLn $ ppShow hist
 
 main :: IO ()
 main = test1
